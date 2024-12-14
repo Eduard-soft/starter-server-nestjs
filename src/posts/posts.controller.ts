466e6600cbfa7515ce3363a555e1c03b86cc0d62
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2024-12-14 12:41:01 +0300

    created relation post-user
    Signed-off-by: Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,33 +1,55 @@
-import { Body, Controller, Post, Get, Patch, Param, ParseIntPipe, Delete } from '@nestjs/common';
-import { PostsService } from './posts.service';
-import { CreatePostDto } from './dtos/create-post.dto';
-import { Post as PostModel } from '@prisma/client';
-import { UpdatePostDto } from './dtos/update-post.dto';
-// import { Post as PrismaService } from '@prisma/client';
+import { Body, 
+         Controller, 
+         Post, 
+         Get, 
+         Patch, 
+         Param, 
+         ParseIntPipe, 
+         Delete, 
+         UseGuards
+} from '@nestjs/common';
+import { PostsService } from './posts.service'
+import { CreatePostDto } from './dtos/create-post.dto'
+import { Post as PostModel } from '@prisma/client'
+import { UpdatePostDto } from './dtos/update-post.dto'
+// import { Post as PrismaService } from '@prisma/client'
+import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard'
+import { CurrentUser } from 'src/utils/decorators/current-user.decorator'
 
+@UseGuards(JwtAccessGuard)
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  async get() {
-    return await this.postsService.get()
+  async get(
+    @CurrentUser("id", ParseIntPipe) userId: number
+    ):Promise<PostModel[]> {
+    return await this.postsService.get(userId)
   }
 
   @Post()
-  async createOne(@Body() dto: CreatePostDto): Promise<PostModel> {
-    return await this.postsService.createOne(dto)
+  async createOne(
+    @Body() dto: CreatePostDto,
+    @CurrentUser("id", ParseIntPipe) userId: number
+  ): Promise<PostModel> {
+    return await this.postsService.createOne(dto, userId)
   }
 
   @Patch(":id")
 	async updateOne(
 		@Param("id", ParseIntPipe) id: number,
-		@Body() dto: UpdatePostDto) {
-		return await this.postsService.updeteOne(id, dto)
+		@Body() dto: UpdatePostDto,
+    @CurrentUser("id", ParseIntPipe) userId: number
+  ): Promise<PostModel> {
+		return await this.postsService.updateOne(id, dto, userId)
 	}
 
 	@Delete(":id")
-	async deleteOne(@Param("id", ParseIntPipe) id: number) {
-		return await this.postsService.deleteOne(id)
+	async deleteOne(
+    @Param("id", ParseIntPipe) id: number,
+    @CurrentUser("id", ParseIntPipe) userId: number
+  ): Promise<PostModel> {
+		return await this.postsService.deleteOne(id, userId)
 	}
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,52 +3,59 @@ import { CreatePostDto } from './dtos/create-post.dto';
 import { PrismaService } from 'prisma/prisma.service';
 import { Post } from '@prisma/client';
 import { UpdatePostDto } from './dtos/update-post.dto';
+import { Post as PostModel } from '@prisma/client'
 
 @Injectable()
 export class PostsService {
 
 	constructor( private readonly prismaService: PrismaService) {}
 
-	async get() {
-		return await this.prismaService.post.findMany()
+	async get(userId: number) {
+		return await this.prismaService.post.findMany({
+			where: { userId}
+		})
 	}
 
-	async createOne(dto: CreatePostDto): Promise<Post> {
-		const post = await this.prismaService.post.create({
+	async createOne({ title, content}: CreatePostDto, userId: number): Promise<Post> {
+		const createPost = await this.prismaService.post.create({
+			data: {
+				title,
+				content,
+				userId
+			}
+		})
+
+		return createPost
+	}
+
+	async updateOne(id: number, dto: UpdatePostDto, userId: number) {
+		await this.getOneOrThrow(id, userId)
+
+		const updatedPost = await this.prismaService.post.update({
+			where: { id, userId },
 			data: dto
 		})
 
-		return post
+		return updatedPost
 	}
 
-	async updeteOne(id: number, dto: UpdatePostDto) {
-		await this.getOneOrThrow(id)
-
-		const updetedPost = await this.prismaService.post.update({
-			where: { id },
-			data: dto
-		})
-
-		return updetedPost
-	}
-
-	async deleteOne(id: number) {
-		await this.getOneOrThrow(id)
+	async deleteOne(id: number, userId: number) {
+		await this.getOneOrThrow(id, userId)
 
 		const deletePost = await this.prismaService.post.delete({
-			where: { id }
+			where: { id, userId }
 		})
 		return deletePost
 	}
 
 		// Private methods
-		private async getOneOrThrow(id: number) {
-			const task = await this.prismaService.post.findUnique({ where: { id } })
+		private async getOneOrThrow(id: number, userId?: number): Promise<PostModel> {
+			const post = await this.prismaService.post.findUnique({ where: { id } })
 	
-			if (!task) {
-				throw new NotFoundException("Could not find any task")
+			if (!post) {
+				throw new NotFoundException("Could not find any post")
 			}
 	
-			return task
+			return post
 		}
 }
